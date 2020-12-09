44a2413b91d2497d1fdfe0fe51dee63fe1e906a7
Duarte-Rodrigues <58471052+Duarte-Rodrigues@users.noreply.github.com>
2020-12-09 23:03:49 +0000

    minor minor changes

--- html/login.php
+++ html/login.php
@@ -16,7 +16,7 @@ if (isset($_SESSION['id'])){
 
 <head>
     <meta charset="UTF-8">
-    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="css/style.css" rel="stylesheet">
     <link href="css/layout.css" rel="stylesheet">
     <link href="css/login.css" rel="stylesheet">
