8a68c4246d2c6fc17a7b815e09227981052666b8
Duarte-Rodrigues <58471052+Duarte-Rodrigues@users.noreply.github.com>
2020-12-28 14:14:03 +0000

    pagination final responsiveness and arrows

--- html/templates/pagination_tpl.php
+++ html/templates/pagination_tpl.php
@@ -3,23 +3,29 @@
     function addPastPagination() { ?>
         <div id="pagination">
             <?php if ($_SESSION['past_page'] > 1) { ?>
-                <a href="?past_page=<?php echo $_SESSION['past_page']-1 ?>&future_page=<?php echo $_SESSION['future_page'] ?>"> &lt; </a>
-            <?php }
-            echo $_SESSION['past_page']; ?>
+                <a href="?past_page=<?php echo 1 ?>&future_page=<?php echo $_SESSION['future_page'] ?>"> <i class="fa fa-angle-double-left"></i> </a>
+                <a class="arrows" href="?past_page=<?php echo $_SESSION['past_page']-1 ?>&future_page=<?php echo $_SESSION['future_page'] ?>"> <i class="fa fa-angle-left"></i> </a>
+            <?php } ?>
+
+            <?php echo $_SESSION['past_page']; ?>
+
             <?php if ($_SESSION['past_page'] < $_SESSION['max_past']) { ?>
-                <a href="?past_page=<?php echo $_SESSION['past_page']+1 ?>&future_page=<?php echo $_SESSION['future_page'] ?>"> &gt; </a>
-                <?php } ?>
+                 <a class="arrows" href="?past_page=<?php echo $_SESSION['past_page']+1 ?>&future_page=<?php echo $_SESSION['future_page'] ?>"> <i class="fa fa-angle-right"></i> </a> <!-- &#x21E8; -->
+                <a  href="?past_page=<?php echo $_SESSION['max_past'] ?>&future_page=<?php echo $_SESSION['future_page'] ?>"> <i class="fa fa-angle-double-right"></i> </a>
+                 <?php } ?>
         </div>
     <?php }
 
     function addFuturePagination() { ?>
         <div id="pagination">
             <?php if ($_SESSION['future_page'] > 1) { ?>
-                <a href="?past_page=<?php echo $_SESSION['past_page'] ?>&future_page=<?php echo $_SESSION['future_page']-1 ?>"> &lt; </a>
+                <a class="arrows" href="?past_page=<?php echo $_SESSION['past_page'] ?>&future_page=<?php echo 1 ?>"> <i class="fa fa-angle-double-left"></i> </a>
+                <a class="arrows" href="?past_page=<?php echo $_SESSION['past_page'] ?>&future_page=<?php echo $_SESSION['future_page']-1 ?>"> <i class="fa fa-angle-left"></i> </a>
             <?php }
             echo $_SESSION['future_page']; ?>
             <?php if ($_SESSION['future_page'] < $_SESSION['max_future']) { ?>
-                <a href="?past_page=<?php echo $_SESSION['past_page'] ?>&future_page=<?php echo $_SESSION['future_page']+1 ?>"> &gt; </a>
+                <a class="arrows" href="?past_page=<?php echo $_SESSION['past_page'] ?>&future_page=<?php echo $_SESSION['future_page']+1 ?>"> <i class="fa fa-angle-right"></i> </a>
+                <a class="arrows" href="?past_page=<?php echo $_SESSION['past_page'] ?>&future_page=<?php echo $_SESSION['max_future'] ?>"> <i class="fa fa-angle-double-right"></i> </a>
                 <?php } ?>
         </div>
     <?php }
